15aea21fa6c36fd352f43fd220b715f85139e76b
asauchuk <51837757+asauchuk@users.noreply.github.com>
2019-06-24 12:01:58 +0300

    Интеллектуальный логин

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,7 +6,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="qwerty", header="qwertyheader", footer="qwertyfooter"))
-    app.session.logout()
+
 
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
